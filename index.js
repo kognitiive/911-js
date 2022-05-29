136ff61bbfb83307bbd01dc17b9bbc81aa88b595
Ivan Shvayka <shvaykaofficial@gmail.com>
2022-05-29 11:23:19 +0300

    task-2

--- index.js
+++ index.js
@@ -5,16 +5,35 @@
 //то показать через alert: "Верно!"
 //в противном случае отобразить:"Не знаете? ECMAScript!"
 
-const answer = prompt('Яка офіційна назва JavaScript?');
-// if (answer === "ECMAScript") {
-//     alert("Вірно")
-// } else {
-//     alert("Не знаете? ECMAScript!")
-// }
-// let message = 'Не знаете? ECMAScript!';
+// const answer = prompt('Яка офіційна назва JavaScript?');
+// // if (answer === "ECMAScript") {
+// //     alert("Вірно")
+// // } else {
+// //     alert("Не знаете? ECMAScript!")
+// // }
+// // let message = 'Не знаете? ECMAScript!';
 
-// answer === 'ECMAScript' ? message = 'Вірно' : message;
+// // answer === 'ECMAScript' ? message = 'Вірно' : message;
 
-// alert(message);
+// // alert(message);
 
-alert(answer === 'ECMAScript' ? 'Вірно' : 'Не знаете? ECMAScript!');
+// alert(answer === 'ECMAScript' ? 'Вірно' : 'Не знаете? ECMAScript!');
+
+// 2 && 1 && null && 0 && undefined
+// console.log(2 && 1 && null && 0 && undefined);
+
+//Напишите программу, которая получит от пользователя
+//число (количество минут) и выведет в консоль
+//строку в формате часов и минут
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+// 70 === 01:10
+const globalMinutes = 90;
+let hours = Math.floor(globalMinutes / 60);
+let minutes = globalMinutes % 60;
+minutes = String(minutes).padStart(2, "0");
+
+// hours.padStart(2, "0");
+hours = String(hours).padStart(2, "0");
+console.log(`${hours} : ${minutes}`);
+
+
